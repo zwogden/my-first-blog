079ef07dd9fd9de9bb80d25843f38526db47a0c9
zwogden <zwogden@gmail.com>
2017-10-14 15:15:00 -0700

    Added login.

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,6 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
+LOGIN_REDIRECT_URL = '/'
+ 
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Los_Angeles'
